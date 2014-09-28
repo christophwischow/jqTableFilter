2bae7ef099957f7d41ab19f38bc7156ff2bb8bc8
Benedikt Olek <benedikt@olek.biz>
2014-09-28 22:21:56 +0200

    added the plugin function - minimal code

--- jqtablefilter.js
+++ jqtablefilter.js
@@ -1,3 +1,9 @@
 (function($) {
+  $.fn.jqTableFilter = function() {
+    // object the plugin was called on
+    var table = this;
 
+    // preserve chainability
+    return table;
+  }
 }(jQuery));
